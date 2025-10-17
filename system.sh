d7ed32918fd161d8512612eb85f12021fc918be7
chellmann <c_h@posteo.de>
2025-10-17 15:08:20 +0200

    Clean up system.sh by removing unused device case
    Removed commented-out line for pico-plus-sd device.

--- system.sh
+++ system.sh
@@ -27,7 +27,6 @@ popd || exit
 
 rm -rf .BoardConfig.mk
 case $DEVICE_NAME in
-  # pico-plus-sd) ln -s project/cfg/BoardConfig_IPC/BoardConfig-SD_CARD-Buildroot-RV1103_Luckfox_Pico_Plus-IPC.mk .BoardConfig.mk ;;
   pico-plus-flash) ln -s project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-Buildroot-RV1103_Luckfox_Pico_Plus-IPC.mk .BoardConfig.mk ;;
   *)
     echo "Invalid device: ${DEVICE_NAME}."
