8f79a46c932b2291ff7440213d4e365b93cd65d3
chellmann <c_h@posteo.de>
2025-10-26 18:08:50 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,22 +44,22 @@ apk add i2c-tools
 apk add git
 apk add python3
 apk add nano
-#apk add py3-pip
+apk add py3-pip
 
 # python3 venv, install packages
-#mkdir -p /opt/veloxsensor3
-#python3 -m venv /opt/veloxsensor3
-#source /opt/veloxsensor3/bin/activate
+mkdir -p /opt/veloxsensor3
+python3 -m venv /opt/veloxsensor3
+source /opt/veloxsensor3/bin/activate
 
 #echo "show disk usage and list /extrootfs"
 ##df -h
 #ls -alh /opt
 
-#pip install smbus2
-#pip install w1thermsensor[async]
-#pip install w1thermsensor
-#pip install pymodbus[serial]
-#deactivate
+pip install smbus2
+pip install w1thermsensor[async]
+pip install w1thermsensor
+pip install pymodbus[serial]
+deactivate
 
 #echo "show disk usage and list /extrootfs"
 #df -h
